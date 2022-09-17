23ce0c19c65cba925de2ec7b19c99c81198c63a0
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2022-09-17 16:26:34 +0530

    correctAnswer array updated

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector('.quizform');
 const sumbitAnswerBtn = document.querySelector("#sumbit");
 const output = document.querySelector('#output');
 
-const correctAnswers = ["Right", ""]
+const correctAnswers = ["A geometric figure","Polygon","3","Equilateral","Geometry","2","Scalene","3","Isosceles","Hypotenuse"]
 
 function calculateScore() {
     let score = 0;
@@ -15,7 +15,7 @@ function calculateScore() {
         index = index + 1;
     }
     console.log(score)
-    if(score>=2){
+    if(score>=8){
         output.innerText ="Your score is"+" "+ score +" "+ "keep it up 👍"
     }else{
         output.innerText ="Your score is"+" "+ score +" "+ "try again 💪"
